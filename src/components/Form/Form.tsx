1bf2303d7f5fdfe37db12bdaa0f35ed1f5171b61
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-29 18:54:27 -0600

    Validando el formulario

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,16 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from "../Form/Form.module.css";
 import { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 export default function Form() {
   const [search, setSearch] = useState<SearchType>({
     city: "",
     country: "",
   });
+
+  const [alert, setAlert] = useState("");
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
@@ -18,8 +21,17 @@ export default function Form() {
     });
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (Object.values(search).includes("")) {
+      setAlert("Todos los campos son obligatorios.");
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input
