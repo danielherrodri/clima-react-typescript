36eef3a0ab4b526b5a72b4ab646768f0479a8c22
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-29 20:24:28 -0600

    Tipar los datos de la consulta con Valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,15 +1,42 @@
 import axios from 'axios'
+// import { z } from 'zod'
 import { SearchType, Weather } from '../types'
+import { object, number, string, InferOutput, parse } from 'valibot'
 
-function isWeatherResponse(weather: unknown): weather is Weather {
-    return (
-        Boolean(weather) && typeof weather === 'object' &&
-        typeof (weather as Weather).name === 'string' &&
-        typeof (weather as Weather).main.temp === 'number' &&
-        typeof (weather as Weather).main.temp_max === 'number' &&
-        typeof (weather as Weather).main.temp_min === 'number'
-    )
-}
+//TYPE GUARD OR ASSERTION
+// function isWeatherResponse(weather: unknown): weather is Weather {
+//     return (
+//         Boolean(weather) && typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number'
+//     )
+// }
+
+//ZOD
+// const Weather = z.object({
+//     name: z.string(),
+//     main: z.object({
+//         temp: z.number(),
+//         temp_max: z.number(),
+//         temp_min: z.number(),
+//     })
+// })
+
+// type Weather = z.infer<typeof Weather>
+
+//Valibot
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
+    })
+})
+
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -25,13 +52,12 @@ export default function useWeather() {
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
             const { data: weatherResult } = await axios.get(weatherUrl)
 
-            const result = isWeatherResponse(weatherResult)
+            // const result = isWeatherResponse(weatherResult) //TYPE GUARD
+            // const result = Weather.safeParse(weatherResult) // ZOD
+            const result = parse(WeatherSchema, weatherResult)
+            console.log(result)
 
-            if (result) {
-                console.log(weatherResult.name)
-            } else {
-                console.log('El objeto está mal formado')
-            }
+
 
         } catch (error) {
             console.log(error)
